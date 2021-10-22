1efd9accca2bb52c5941cabd2fe95c3f75e972e0
007tom <zhyd007@gmail.com>
2021-10-22 13:25:49 +0800

    feat: init client.

---
+++ client/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    hot: true,
+    open: true,
+  },
+  optimization: {
+    runtimeChunk: true,
+    moduleIds: "deterministic",
+  },
+});
