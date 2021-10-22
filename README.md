f71dfe75091bbcda92c9112d53efcedc2ab60bd7
Yadong Zhang <zhyd007@gmail.com>
2021-10-22 13:30:29 +0800

    feat: init client. (#1)
    * feat: added codesandbox config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+![GitHub](https://img.shields.io/github/license/zhyd1997/Kirin)
+
 > Migrate from [Eorg](https://github.com/zhyd1997/Eorg) and [Eorg-Server](https://github.com/zhyd1997/Eorg-Server).
---
+++ client/webpack.common.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPluginv = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: path.resolve("./src/index.tsx"),
+  plugins: [
+    new CleanWebpackPlugin(),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new ForkTsCheckerWebpackPluginv({
+      eslint: {
+        files: "./src/**/*.{ts,tsx,js,jsx}", // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+      },
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        include: path.resolve(__dirname, "src"),
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
